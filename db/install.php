d66fac4262e816fa73a6fa0d741b488428e2cb35
Carlos Eduardo Alves <kmiksi@gmail.com>
2014-05-27 18:20:53 -0300

    correct DB_install error during instalation

--- db/install.php
+++ db/install.php
@@ -1,3 +1,13 @@
 <?php
 
+/**
+ * Component 'simple_contact_form'.
+ *
+ * @package   simple_contact_form
+ * @copyright 2014 Rodrigo Saraiva  {@link http://moodle.com}, Carlos Eduardo Alves {@link http://github.com/kmiksi}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+function xmldb_local_simple_contact_form_install() {
+    global $CFG, $DB;
+    return true;
 }
