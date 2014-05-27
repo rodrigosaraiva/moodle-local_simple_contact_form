b64b476f71586a41fb4ffa5180e4bbd242a51b37
Carlos Eduardo Alves <kmiksi@gmail.com>
2014-05-27 18:22:30 -0300

    Version Updated: - removed "city_state" option - configurable dinamic "city" and "state" options added - user can input your city info when not listed - review and various improvements - fully internationalized

--- version.php
+++ version.php
@@ -1,3 +1,24 @@
 <?php
-$plugin->version  = 2014043000;   // The (date) version of this plugin
-$plugin->requires = 2013110500;   // Requires this Moodle version
+
+/**
+ * Version info for 'simple_contact_form' plugin.
+ *
+ * @package   simple_contact_form
+ * @copyright 2014 Rodrigo Saraiva  {@link http://moodle.com}, Carlos Eduardo Alves {@link http://github.com/kmiksi}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$plugin->version = 2014052700;   // The (date) version of this plugin
+/*
+ * Moodle 1.9 = 2007101509
+ * Moodle 2.0 = 2010112400
+ * Moodle 2.1 = 2011070100
+ * Moodle 2.2 = 2011120500
+ * Moodle 2.3 = 2012062500
+ * Moodle 2.4 = 2012120300
+ * Moodle 2.5 = 2013051400
+ * Moodle 2.6 = 2013111800
+ * Moodle 2.7 = 2014051200
+ * @see http://docs.moodle.org/dev/Moodle_Versions
+ */
+$plugin->requires = 2013051400; // Requires this Moodle version
+$plugin->maturity = MATURITY_RC; //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
